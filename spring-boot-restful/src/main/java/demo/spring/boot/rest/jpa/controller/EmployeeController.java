84da94c6509ff8b76112bd3d85c5e3cc9015e886
David <dwilfred@gmail.com>
2017-12-21 09:58:43 +0530

    Added Service

--- spring-boot-restful/src/main/java/demo/spring/boot/rest/jpa/controller/EmployeeController.java
+++ spring-boot-restful/src/main/java/demo/spring/boot/rest/jpa/controller/EmployeeController.java
@@ -8,28 +8,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import demo.spring.boot.rest.jpa.entity.Employee;
-import demo.spring.boot.rest.jpa.repository.EmployeeRepository;
+import demo.spring.boot.rest.jpa.service.EmployeeService;
 
 @Controller    
 @RequestMapping(path="/employee") 
 public class EmployeeController {
 
 	@Autowired
-	private EmployeeRepository employeeRepository;
+	private EmployeeService employeeService;
 
 	@GetMapping(path="/add") 
-	public @ResponseBody Iterable<Employee> addNewEmployee(@RequestParam String firstName, @RequestParam String lastName
+	public @ResponseBody Iterable<Employee> addEmployee(@RequestParam String firstName, @RequestParam String lastName
 			, @RequestParam String email) {
 		Employee newEmployee = new Employee();
 		newEmployee.setFirstName(firstName);
 		newEmployee.setLastName(lastName);
 		newEmployee.setEmail(email);
-		employeeRepository.save(newEmployee);
+		employeeService.addEmployee(newEmployee);
 		return getAllEmployees();
 	}
 
 	@GetMapping(path="/all")
 	public @ResponseBody Iterable<Employee> getAllEmployees() {
-		return employeeRepository.findAll();
+		return employeeService.getAllEmployees();
 	}
 }
